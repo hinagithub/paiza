182c6a244805d5fd14d8cecd88e91ca6ba794679
hina <matsunohinako@matsunohinakonoMacBook-Pro.local>
2025-04-22 06:04:31 +0900

    Add

---
+++ golang/level-c/3/step2.go
@@ -0,0 +1,9 @@
+// 文字列 paiza と learning を半角スペース区切りで出力してください。
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("paiza learning")
+}
---
+++ golang/level-c/3/step3.go
@@ -0,0 +1,19 @@
+// 2 つの文字列 S, T が入力されます。S, T を改行区切りで出力してください。
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	sc := bufio.NewScanner(os.Stdin)
+	sc.Scan()
+	text1 := sc.Text()
+	sc.Scan()
+	text2 := sc.Text()
+	fmt.Println(text1)
+	fmt.Println(text2)
+}
