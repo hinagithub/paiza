02dcc8a2a579cab8548329622c9af84b73f9eebf
hina <matsunohinako@matsunohinakonoMacBook-Pro.local>
2025-05-03 04:12:00 +0900

    Add

---
+++ golang/level-c/logic/1/step1.go
@@ -0,0 +1,33 @@
+// 問題
+// 0 または 1 の整数 A と B が与えられます。 A AND B の結果を出力してください。
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+
+	inputs := strings.Fields(scanner.Text())
+	a := atoi(inputs[0])
+	b := atoi(inputs[1])
+
+	fmt.Println(a & b)
+	fmt.Println(0 & 1)
+}
+
+func atoi(text string) int {
+	num, err := strconv.Atoi(text)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "atoi err", err)
+		os.Exit(1)
+	}
+	return num
+}
