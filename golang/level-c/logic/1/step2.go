4a31e064f7f2297a28730e9ab7d01e36902cba63
hina <matsunohinako@matsunohinakonoMacBook-Pro.local>
2025-05-03 04:12:00 +0900

    Add

---
+++ golang/level-c/logic/1/step2.go
@@ -0,0 +1,24 @@
+// 問題
+// 0 または 1 の整数 A と B が与えられます。 A OR B の結果を出力してください。
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	if !scanner.Scan() {
+		fmt.Println(os.Stderr, "scan err")
+		os.Exit(1)
+	}
+	inputs := strings.Fields(scanner.Text())
+	a, _ := strconv.Atoi(inputs[0])
+	b, _ := strconv.Atoi(inputs[1])
+	fmt.Println(a | b)
+}
---
+++ golang/level-c/logic/1/step6.go
@@ -0,0 +1,23 @@
+// 問題
+// 0 または 1 の整数 A と B が与えられます。 A NOR B の結果を出力してください。
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	inputs := strings.Fields(scanner.Text())
+	a, _ := strconv.Atoi(inputs[0])
+	b, _ := strconv.Atoi(inputs[1])
+
+	fmt.Println(1 - (a | b))
+
+}
