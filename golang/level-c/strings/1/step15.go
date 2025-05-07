368cbc2625d2b52a9c6fc97cb038bf1105d4d49c
hina <matsunohinako@matsunohinakonoMacBook-Pro.local>
2025-05-08 06:32:38 +0900

    Add

---
+++ golang/level-c/strings/1/step15.go
@@ -0,0 +1,32 @@
+// 問題
+// 大文字と小文字のアルファベットが混ざった文字列 S が与えられます。
+// S の小文字を全て大文字に、大文字を全て小文字にした文字列を出力してください。
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"unicode"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	if !scanner.Scan() {
+		fmt.Fprintf(os.Stderr, "scan err")
+		os.Exit(1)
+	}
+	text := scanner.Text()
+	result := strings.Map(swap, text)
+	fmt.Println(string(result))
+}
+
+func swap(r rune) rune {
+	if unicode.IsUpper(r) {
+		return unicode.ToLower(r)
+	} else {
+		return unicode.ToUpper(r)
+	}
+}
