815b682d83f3c1ccdf2689a254080c8995306081
hinagithub <matsuno.hinako@gmail.com>
2025-06-02 09:43:23 +0900

    Add

--- golang/level-c/roop/3/step4/main.go
+++ golang/level-c/roop/3/step4/main.go
@@ -11,11 +11,10 @@ import (
 func main() {
 	var n int
 	fmt.Scan(&n)
-	for i := 0; i < n; i++ {
-		square := 2
-		for j := 0; j < i; j++ {
-			square *= 2
-		}
-		fmt.Println(square)
+
+	result := 2
+	for i := 1; i <= n; i++ {
+		fmt.Println(result)
+		result *= 2
 	}
 }
