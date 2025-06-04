1f0fe750e8ac907149372397e075c0ada380c670
hinagithub <matsuno.hinako@gmail.com>
2025-06-05 04:05:58 +0900

    Add

---
+++ golang/level-c/roop2/2/step9/main.go
@@ -0,0 +1,19 @@
+// 問題
+// 整数 N が与えられます。
+// N の階乗 N! を計算して出力してください。
+
+package main
+
+import "fmt"
+
+func main() {
+
+	var n int
+	fmt.Scan(&n)
+
+	result := 1
+	for i := 1; i < n; i++ {
+		result += result * i
+	}
+	fmt.Println(result)
+}
