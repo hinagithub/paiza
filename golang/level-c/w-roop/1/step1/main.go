56a2698d0fb7f8eb7450b75b7b5a1e380a738d67
hina <44778704+hinagithub@users.noreply.github.com>
2025-06-07 20:53:13 +0900

    Fix

--- golang/level-c/w-roop/1/step1/main.go
+++ golang/level-c/w-roop/1/step1/main.go
@@ -1,12 +1,17 @@
 // 正の整数 N が与えられるので、1 〜 N の整数を 1 から順に半角スペース区切りで 1 行で出力してください。
 
 package main
-import "fmt"
+import (
+    "fmt"
+    "strings"
+    "strconv"
+)
 func main(){
     var n int
     fmt.Scan(&n)
-    
+    strNumbers := []string{}
     for i := 1; i <= n; i++ {
-        fmt.Println(i)
+        strNumbers = append(strNumbers, strconv.Itoa(i))
     }
+    fmt.Println(strings.Join(strNumbers, " "))
 }
